232d09d345e76ef9eca369ab5d00c3deaa6860e4
abhi5244 <55691556+abhi5244@users.noreply.github.com>
2019-11-06 15:13:46 +0000

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[15]:
+
+class student:
+    def _int_(self, studentId, coursecode): 
+        Student.StudentId = StudentId
+        Student.CourseName = CourseName
+        
+class studentProcessing:
+    dictStudents= dict()
+    def Add(self, student): 
+        StudentProcessing.dictStudents.update({Student.StudentId: Student.CourseName})
+
+
+# In[ ]:
+
+
+
