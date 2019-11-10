aa947d7d19f5c1a321fcbf12842aa3a1e16ae971
abhi5244 <55691556+abhi5244@users.noreply.github.com>
2019-11-10 11:23:36 +0000

    Add files via upload

---
+++ Untitled7 (1).py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+class Student:
+    dictStudents = dict()
+    sortedDictStudents = dict()
+    firstStudentDict = dict()
+    def __init__(self, StudentId, CourseName):
+        Student.StudentId = StudentId
+        Student.CourseName = CourseName
+
